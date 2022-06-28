ad38f9dfad0600ae3adfc04cb9a9fa9debe507b2
Anna Jeżowska <annaje@Annas-MacBook-Air.local>
2022-06-28 20:37:26 +0200

    main-header

---
+++ scripts.js
@@ -0,0 +1,15 @@
+////https://www.superlist.com/
+
+//hero-header js
+
+const left = document.getElementById("left-side");
+
+const handleMove = (e) => {
+  left.style.width = `${(e.clientX / window.innerWidth) * 100}%`;
+};
+
+document.onmousemove = (e) => handleMove(e);
+
+document.ontouchmove = (e) => handleMove(e.touches[0]);
+
+// end hero-header js
